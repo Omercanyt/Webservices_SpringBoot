61dbd314cfad22880dc9432042b92654af2fe0cb
Omercanyt <omercanyt@gmail.com>
2021-05-11 18:40:34 +0300

    Initial commit

---
+++ src/test/java/com/team14/WebService/Team14WebService457ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team14.WebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team14WebService457ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

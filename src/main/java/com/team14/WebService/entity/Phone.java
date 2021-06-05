e4994b9b41a85fa228c3b31678e982f354e18bf2
Günseli Sevinç <51919213+gunselisevinc@users.noreply.github.com>
2021-06-05 21:51:15 +0300

    phone entity refactoring according to new design (with phone features)

--- src/main/java/com/team14/WebService/entity/Phone.java
+++ src/main/java/com/team14/WebService/entity/Phone.java
@@ -3,43 +3,96 @@ package com.team14.WebService.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "Phone")
 public class Phone {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="phone_id")
     private int phone_id;
-
-    @Column(name="phone_model")
     private String phone_model;
-
-    @Column(name="phone_brand")
     private String phone_brand;
-
-    @Column(name="phone_screenSize")
     private String phone_screenSize;
-
-    @Column(name="phone_internalMemory")
     private String phone_internalMemory;
-
-    @Column(name="phone_price")
     private float price;
+    @OneToMany(mappedBy = "phoneR", cascade = CascadeType.ALL)
+    private List<Rating> phoneRatings;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "phonefeatureRelation", joinColumns = @JoinColumn(name="phone_fkid"),inverseJoinColumns = @JoinColumn(name = "phonefeaturefkID"))
+    private List<phoneFeatures> phofeatures = new ArrayList<>();
 
-    @OneToMany(targetEntity = Rating.class, cascade = CascadeType.ALL)
-    @JoinColumn(name = "phone_fk", referencedColumnName = "phone_id")
-    private List<Rating> ratings;
 
-    @ElementCollection
-    @CollectionTable(
-            name = "PhoneFeatures",
-            joinColumns = @JoinColumn(name = "phone_id")
-    )
-    private List<String> phone_features;
+    public int getPhone_id() {
+        return phone_id;
+    }
+
+    public void setPhone_id(int phone_id) {
+        this.phone_id = phone_id;
+    }
+
+    public String getPhone_model() {
+        return phone_model;
+    }
+
+    public void setPhone_model(String phone_model) {
+        this.phone_model = phone_model;
+    }
+
+    public String getPhone_brand() {
+        return phone_brand;
+    }
+
+    public void setPhone_brand(String phone_brand) {
+        this.phone_brand = phone_brand;
+    }
+
+    public String getPhone_screenSize() {
+        return phone_screenSize;
+    }
+
+    public void setPhone_screenSize(String phone_screenSize) {
+        this.phone_screenSize = phone_screenSize;
+    }
+
+    public String getPhone_internalMemory() {
+        return phone_internalMemory;
+    }
+
+    public void setPhone_internalMemory(String phone_internalMemory) {
+        this.phone_internalMemory = phone_internalMemory;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public List<Rating> getPhoneRatings() {
+        return phoneRatings;
+    }
+
+    public void setPhoneRatings(List<Rating> phoneRatings) {
+        this.phoneRatings = phoneRatings;
+    }
+
+    public List<phoneFeatures> getPhofeatures() {
+        return phofeatures;
+    }
+
+    public void setPhofeatures(List<phoneFeatures> phofeatures) {
+        this.phofeatures = phofeatures;
+    }
 }

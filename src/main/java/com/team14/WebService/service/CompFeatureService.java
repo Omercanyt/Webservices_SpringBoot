9b27a157a44729f67a346c15132f9dcb328e1376
Omercanyt <omercanyt@gmail.com>
2021-06-05 21:57:39 +0300

    Implemented the service part of our system in ComputerFeatures

--- src/main/java/com/team14/WebService/service/CompFeatureService.java
+++ src/main/java/com/team14/WebService/service/CompFeatureService.java
@@ -1,4 +1,19 @@
 package com.team14.WebService.service;
 
+import com.team14.WebService.entity.computerFeatures;
+import com.team14.WebService.repository.CompFeatureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CompFeatureService {
+    @Autowired
+    CompFeatureRepository compFeatureRepository;
+
+    public computerFeatures saveCompFeature(computerFeatures cf){
+        return compFeatureRepository.save(cf);
+    }
+    public computerFeatures getCompFeature(int id){
+        return compFeatureRepository.findById(id).orElse(null);
+    }
 }

eb5d9e219996a98d7dae5747265cb81b1f8415ce
Omercanyt <omercanyt@gmail.com>
2021-06-05 22:07:57 +0300

    Implemented the ratingservice service class

--- src/main/java/com/team14/WebService/service/RatingService.java
+++ src/main/java/com/team14/WebService/service/RatingService.java
@@ -1,4 +1,20 @@
 package com.team14.WebService.service;
 
+import com.team14.WebService.entity.Computer;
+import com.team14.WebService.entity.Rating;
+import com.team14.WebService.repository.RatingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RatingService {
+    @Autowired
+    RatingRepository ratingRepository;
+
+    public Rating saveRating(Rating r){
+        return ratingRepository.save(r);
+    }
+    public Rating getRating(int id){
+        return ratingRepository.findById(id).orElse(null);
+    }
 }

bd3ee5c515eb5735e15b35f751f57d6296f237bb
celalcetiner-01 <73341830+celalcetiner-01@users.noreply.github.com>
2021-06-05 22:21:09 +0300

    Update PhoneRepository.java
    phonefeature repository implemented

--- src/main/java/com/team14/WebService/repository/PhoneRepository.java
+++ src/main/java/com/team14/WebService/repository/PhoneRepository.java
@@ -1,4 +1,7 @@
 package com.team14.WebService.repository;
 
-public interface PhoneRepository {
+import com.team14.WebService.entity.Phone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhoneRepository  extends JpaRepository<Phone, Integer> {
 }

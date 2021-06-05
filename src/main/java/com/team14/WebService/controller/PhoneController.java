453cce19a63aa0cf6cd466fe772c0c274e213de2
Günseli Sevinç <51919213+gunselisevinc@users.noreply.github.com>
2021-06-05 21:56:10 +0300

    phone controller implemented

--- src/main/java/com/team14/WebService/controller/PhoneController.java
+++ src/main/java/com/team14/WebService/controller/PhoneController.java
@@ -1,4 +1,34 @@
 package com.team14.WebService.controller;
 
+import com.team14.WebService.entity.Computer;
+import com.team14.WebService.entity.Phone;
+import com.team14.WebService.service.PhoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class PhoneController {
+    @Autowired
+    PhoneService phoneService;
+
+    @PostMapping("/addPhone")
+    public Phone savePhone(@RequestBody Phone r){
+        return phoneService.savePhone(r);
+    }
+    @GetMapping("/getPhone/{id}")
+    public Phone getPhone(@PathVariable int id){
+        return phoneService.getPhone(id);
+    }
+    @GetMapping("/getAllPhone")
+    public List<Phone> getAllPhone(){
+        return phoneService.getAllPhone();
+    }
+
+    @GetMapping("/getPhoneByBrand/{brand}")
+    public List<Phone> getPhoneByBrand(@PathVariable String brand){
+        return phoneService.getPhoneByBrand(brand);
+    }
+
 }

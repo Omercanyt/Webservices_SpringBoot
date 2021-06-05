e5cd12eec41f903f7add4ad65d02fab6d5c53958
Günseli Sevinç <51919213+gunselisevinc@users.noreply.github.com>
2021-06-05 21:57:25 +0300

    phone feature implemented

--- src/main/java/com/team14/WebService/controller/PhoneFeatureController.java
+++ src/main/java/com/team14/WebService/controller/PhoneFeatureController.java
@@ -1,4 +1,21 @@
 package com.team14.WebService.controller;
 
+import com.team14.WebService.entity.phoneFeatures;
+import com.team14.WebService.service.PhoneFeatureService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class PhoneFeatureController {
+    @Autowired
+    PhoneFeatureService phoneFeatureService;
+
+    @PostMapping("/addPhoneFeature")
+    public phoneFeatures savePhoneFeature(@RequestBody phoneFeatures r){
+        return phoneFeatureService.savePhoneFeature(r);
+    }
+    @GetMapping("/getPhoneFeature/{id}")
+    public phoneFeatures getPhoneFeature(@PathVariable int id){
+        return phoneFeatureService.getPhoneFeature(id);
+    }
 }

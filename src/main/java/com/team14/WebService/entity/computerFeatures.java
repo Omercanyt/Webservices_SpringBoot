127e3e1f77fbfdd07afbc5788a7daeec1f577597
celalcetiner-01 <73341830+celalcetiner-01@users.noreply.github.com>
2021-06-05 22:12:08 +0300

    Update computerFeatures.java
    computer feature implemented

--- src/main/java/com/team14/WebService/entity/computerFeatures.java
+++ src/main/java/com/team14/WebService/entity/computerFeatures.java
@@ -1,4 +1,49 @@
 package com.team14.WebService.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
 public class computerFeatures {
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private int compfeatureID;
+    private String compFeatureName;
+    @ManyToMany(mappedBy = "cofeatuers", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    private Set<Computer> cofets = new HashSet<>();
+
+    public int getCompfeatureID() {
+        return compfeatureID;
+    }
+
+    public void setCompfeatureID(int compfeatureID) {
+        this.compfeatureID = compfeatureID;
+    }
+
+    public String getCompFeatureName() {
+        return compFeatureName;
+    }
+
+    public void setCompFeatureName(String compFeatureName) {
+        this.compFeatureName = compFeatureName;
+    }
+
+    public Set<Computer> getCofets() {
+        return cofets;
+    }
+
+    public void setCofets(Set<Computer> cofets) {
+        this.cofets = cofets;
+    }
 }

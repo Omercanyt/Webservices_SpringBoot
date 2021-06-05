7adce98307df31cbf936139a0824561494be7b3c
Omercanyt <omercanyt@gmail.com>
2021-06-05 22:05:50 +0300

    Implemented the phone service class

--- src/main/java/com/team14/WebService/service/PhoneService.java
+++ src/main/java/com/team14/WebService/service/PhoneService.java
@@ -1,4 +1,42 @@
 package com.team14.WebService.service;
 
+import com.team14.WebService.entity.Computer;
+import com.team14.WebService.entity.Phone;
+import com.team14.WebService.entity.phoneFeatures;
+import com.team14.WebService.repository.PhoneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PhoneService {
+    @Autowired
+    PhoneRepository phoneRepository;
+
+    public Phone savePhone(Phone p){
+        return phoneRepository.save(p);
+    }
+    public Phone getPhone(int id){
+        return phoneRepository.findById(id).orElse(null);
+    }
+
+    public List<Phone> getAllPhone(){
+        return phoneRepository.findAll();
+    }
+
+    public List<Phone> getPhoneByBrand(String s){
+        Phone temp = new Phone();
+        List<Phone> phones = new ArrayList<>();
+        List<Phone> phones2 = new ArrayList<>();
+        phones=getAllPhone();
+        for (Phone phone : phones) {
+            if (phone.getPhone_brand().equals(s)) {
+                temp = phone;
+                phones2.add(temp);
+            }
+        }
+        return phones2;
+    }
 }

0dd0684f907be409acc7349ab8bbbb902cd25c57
Omercanyt <omercanyt@gmail.com>
2021-06-27 18:58:43 +0300

    General old project updated
    Its been updated with all members of group through google meet. Since this is just to update old code to make phase 5 work, we commit it together by my computer.

--- src/main/java/com/team14/WebService/repository/RatingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team14.WebService.repository;
-
-import com.team14.WebService.entity.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository  extends JpaRepository<Rating, Integer> {
-}
